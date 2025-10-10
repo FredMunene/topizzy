3b8314a3439fc29e05c270d3da0204c54fe1ad54
FredMunene <munene2050gitonga@gmail.com>
2025-10-10 20:13:52 +0300

    Update Deploy script to pass USDC token and treasury addresses to Airtime contract

--- smart_contracts/script/Deploy.s.sol
+++ smart_contracts/script/Deploy.s.sol
@@ -8,11 +8,25 @@ contract DeployScript is Script {
     function setUp() public {}
 
     function run() public returns (Airtime) {
+        // USDC token addresses
+        
+        address usdcTokenAddress = vm.envAddress("USDC_SEPOLIA_TOKEN_ADDRESS");
+        address treasuryAddress = vm.envAddress("TREASURY_ADDRESS");
+        
+        console.log("Deploying with parameters:");
+        console.log("USDC Token:", usdcTokenAddress);
+        console.log("Treasury:", treasuryAddress);
+
         // Start broadcasting to the specified chain
         vm.startBroadcast();
 
-        // Deploy the Airtime contract
-        Airtime airtime = new Airtime();
+        // Deploy the Airtime contract with USDC token and treasury addresses
+        Airtime airtime = new Airtime(usdcTokenAddress, treasuryAddress);
+        
+        // Log the deployed contract address
+        console.log("\nAirtime contract deployed at:", address(airtime));
+        console.log("Treasury address:", airtime.treasury());
+        console.log("USDC Token address:", airtime.usdcToken());
 
         // Stop broadcasting
         vm.stopBroadcast();
